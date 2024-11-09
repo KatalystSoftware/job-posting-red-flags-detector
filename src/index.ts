b7ed0452db1a5f19c9fbdde4e2ac389dea831f9b
Mikael Siidorow <mikael@siidorow.com>
2024-11-09 17:51:08 +0200

    setup secrets handling

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,52 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 import OpenAI from "openai";
 import { zodResponseFormat } from "openai/helpers/zod";
 import { z } from "zod";
 
-const app = new Hono()
+type Bindings = {
+  OPENAI_API_KEY: string;
+};
 
-const openai = new OpenAI({
-  apiKey: /* todo */,
-});
+const app = new Hono<{ Bindings: Bindings }>();
 
 const HTMLResp = z.object({
-  html: z.string()
+  html: z.string(),
 });
-
 
 const systemMessage = `
 You are given a HTML of a job posting. You need to find redflags and greenflags by wrapping them
  in <span class="highlight-negative" data-description="sign of non competent work space"></span> and <span class="highlight-positive" data-description="environmentally conscious"></span>.
 Be creative with the data-descriptions.
-`
+`;
 
-app.post('/', async (c) => {
-  const body = await c.req.json()
+app.post("/", async (c) => {
+  const apiKey = c.env.OPENAI_API_KEY;
+  const openai = new OpenAI({
+    apiKey,
+  });
+
+  const body = await c.req.json();
 
   if (body.html === undefined) {
-    return c.text('no html')
+    return c.text("no html");
   }
 
-  const input = body.html
-
+  const input = body.html;
 
   const completion = await openai.beta.chat.completions.parse({
     model: "gpt-4o",
     messages: [
-        { role: "system", content: systemMessage },
-        {
-            role: "user",
-            content: input,
-        },
+      { role: "system", content: systemMessage },
+      {
+        role: "user",
+        content: input,
+      },
     ],
     response_format: zodResponseFormat(HTMLResp, "htmlResp"),
   });
 
-  const resp = completion.choices[0].message.parsed
-  return c.text(resp?.html ?? 'no content in response')
-})
+  const resp = completion.choices[0].message.parsed;
+  return c.text(resp?.html ?? "no content in response");
+});
 
-export default app
+export default app;
